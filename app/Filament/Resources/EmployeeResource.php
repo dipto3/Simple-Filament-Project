a2fe7f3bf355f0ff4be16639eca1bfdb9ebdb0ce
Piyal Guho Dipto <diptoguho4@gmail.com>
2024-05-31 19:50:49 +0600

    form section

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -12,19 +12,46 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-
+use Filament\Infolists\Components\Section;
 class EmployeeResource extends Resource
 {
     protected static ?string $model = Employee::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-    protected static ?string $navigationLabel = 'Employee';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('country_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('state_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('city_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('department_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\Section::make('User Name')
+                    ->description('Put the user name details in.')
+                    ->schema([Forms\Components\TextInput::make('first_name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('middle_name')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('last_name')
+                    ->maxLength(255),])->columns(3),  
+                
+                Forms\Components\DatePicker::make('dob')
+                    ->required(),
+                Forms\Components\TextInput::make('zip_code')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\DatePicker::make('joined_at')
+                    ->required(),
             ]);
     }
 
@@ -32,7 +59,44 @@ class EmployeeResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('country_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('state_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('city_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('department_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('first_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('middle_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('last_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('dob')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('zip_code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('joined_at')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 Tables\Filters\TrashedFilter::make(),
