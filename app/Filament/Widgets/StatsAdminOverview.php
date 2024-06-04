30d763397463edda9ff81ec8380d5d2cae2ac603
Piyal Guho Dipto <diptoguho4@gmail.com>
2024-06-04 17:35:08 +0600

    city and state count

--- app/Filament/Widgets/StatsAdminOverview.php
+++ app/Filament/Widgets/StatsAdminOverview.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\City;
 use App\Models\Country;
+use App\Models\State;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -12,6 +14,8 @@ class StatsAdminOverview extends BaseWidget
     {
         return [
             Stat::make('Country', Country::count()),
+            Stat::make('State', State::count()),
+            Stat::make('City', City::count()),
         ];
     }
 }
