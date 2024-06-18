67d3088db318d9f95b36ce4e7125fef63122da97
Piyal Guho Dipto <diptoguho4@gmail.com>
2024-06-18 13:16:36 +0600

    authorization

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -10,33 +10,33 @@ class UserPolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(User $user)
     {
-        //
+        return $user->hasRole('test');
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, User $model): bool
+    public function view(User $user, User $model)
     {
-        //
+         return $user->hasRole('test');
     }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user): bool
+    public function create(User $user)
     {
-        //
+         return $user->hasRole('test');
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, User $model): bool
+    public function update(User $user, User $model)
     {
-        //
+         return $user->hasRole('test');
     }
 
     /**
@@ -44,7 +44,7 @@ class UserPolicy
      */
     public function delete(User $user, User $model): bool
     {
-        //
+         return $user->hasRole('test');
     }
 
     /**
@@ -52,7 +52,7 @@ class UserPolicy
      */
     public function restore(User $user, User $model): bool
     {
-        //
+        return $user->hasRole('test');
     }
 
     /**
@@ -60,6 +60,6 @@ class UserPolicy
      */
     public function forceDelete(User $user, User $model): bool
     {
-        //
+        return $user->hasRole('test');
     }
 }
