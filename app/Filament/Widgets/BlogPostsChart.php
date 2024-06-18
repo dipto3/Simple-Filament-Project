c09c4be826792fb251c747a74108386c452f8212
Piyal Guho Dipto <diptoguho4@gmail.com>
2024-06-19 01:47:56 +0600

    dynamic employee joined chart

--- app/Filament/Widgets/BlogPostsChart.php
+++ app/Filament/Widgets/BlogPostsChart.php
@@ -2,7 +2,11 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Employee;
 use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+ 
 
 class BlogPostsChart extends ChartWidget
 {
@@ -10,15 +14,32 @@ class BlogPostsChart extends ChartWidget
 
     protected function getData(): array
     {
-        return [
-            'datasets' => [
-                [
-                    'label' => 'Employee Joined',
-                    'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
-                ],
+        // return [
+        //     'datasets' => [
+        //         [
+        //             'label' => 'Employee Joined',
+        //             'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+        //         ],
+        //     ],
+        //     'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        // ];
+        $data = Trend::model(Employee::class)
+        ->between(
+            start: now()->startOfYear(),
+            end: now()->endOfYear(),
+        )
+        ->perMonth()
+        ->count();
+ 
+    return [
+        'datasets' => [
+            [
+                'label' => 'Employee Joined',
+                'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
             ],
-            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        ];
+        ],
+        'labels' => $data->map(fn (TrendValue $value) => $value->date),
+    ];
     }
 
     protected function getType(): string
